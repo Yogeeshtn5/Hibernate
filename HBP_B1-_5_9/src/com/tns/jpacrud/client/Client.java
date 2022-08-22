d62775e7aa7263834048df87e5bf3cb4b1532a2f
Yogeeshtn5 <yogeeshtn5@gmail.com>
2022-08-22 17:17:16 +0530

    First commit

---
+++ HBP_B1-_5_9/src/com/tns/jpacrud/client/Client.java
@@ -0,0 +1,34 @@
+package com.tns.jpacrud.client;
+
+import com.tns.jpacrud.entities.Student;
+import com.tns.jpacrud.service.StudentService;
+import com.tns.jpacrud.service.StudentServiceImpl;
+
+public class Client {
+	public static void main(String[] args) 
+	{
+StudentService studentService=new StudentServiceImpl();
+
+Student student1=new Student();
+
+//Insert a student
+/*student1.setStdid(103);
+student1.setSname("Yogeesh");
+studentService.addStudent(student1);*/
+
+
+
+
+
+//Retrieval operation
+student1=studentService.findStudentById(101);
+System.out.println(student1);
+
+//update operation
+student1.setSname("Helma");
+studentService.updateStudent(student1);
+
+//Delete operation
+studentService.removeStudent(student1);
+}
+}
